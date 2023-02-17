9b96d6b30fefc647cc30e8bc9362c158fa0be895
Junaidhkn <junaidhkniazi@gmail.com>
2023-02-17 22:36:27 +0500

    class validator

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,9 +1,11 @@
-import { IsString } from 'class-validator';
+import { IsString, IsEmail, IsOptional } from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
+  @IsOptional()
   name: string;
 
+  @IsEmail()
   @IsString()
   email: string;
 
