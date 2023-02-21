2a63c6b7ead79253a4c5f73ff46eba7facdc9dc4
Junaidhkn <junaidhkniazi@gmail.com>
2023-02-21 16:44:05 +0500

    password  hashing

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,19 +1,43 @@
 import { ForbiddenException, Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
 import { UsersService } from './users.service';
+import { promisify } from 'util';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+
+const scrypt = promisify(_scrypt);
 
 @Injectable()
 export class AuthService {
-  constructor(
-    private prismaService: PrismaService,
-    private usersService: UsersService,
-  ) {}
+  constructor(private usersService: UsersService) {}
 
   async signUp(name: string, email: string, password: string) {
+    // Check If the email Already Exists
     const existingUser = await this.usersService.findByEmail(email);
     if (existingUser) {
       throw new ForbiddenException();
     }
+    // Hash the Users Password
+    // Generate a Salt
+
+    const salt = randomBytes(8).toString('hex');
+
+    // Hash the salt and Password Together
+
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    // Join the Hashed result and the salt together
+
+    const hashedPassword = salt + '/' + hash.toString('hex');
+
+    // Create a New User and Save it
+
+    const user = await this.usersService.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    // Return the User
+    return user;
   }
 
   async signIn(email: string, password: string) {}
